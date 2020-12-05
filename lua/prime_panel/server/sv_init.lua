272d2d779b453cde8081648b71898aeb42543e23
Michael Rizzuto <42839710+MichaelRizzuto@users.noreply.github.com>
2020-12-04 22:27:25 -0500

    API integ progress

--- lua/prime_panel/server/sv_init.lua
+++ lua/prime_panel/server/sv_init.lua
@@ -7,10 +7,44 @@ function requestPanelDataFromAPI()
 	if timer.Exists("PanelAPIRequestTimer") then
 		timer.Remove("PanelAPIRequestTimer")
 	end
-	timer.Create("PanelAPIRequestTimer", 1, 0, function()
-		http.Fetch(PRIME_PANEL_CONFIG.URL .. "/api/?token=" .. PRIME_PANEL_CONFIG.SteamAPIKey .. "&id=" .. PRIME_PANEL_CONFIG.ID,
+
+	-- Warns
+	timer.Create("PanelAPIRequestTimerWarns", 1, 0, function()
+		http.Fetch(PRIME_PANEL_CONFIG.URL .. "/api/?token=" .. PRIME_PANEL_CONFIG.SteamAPIKey .. "&id=" .. PRIME_PANEL_CONFIG.ID .. "&warns=true",
 			function(body, length, headers, code)
 				print(body)
+				local warnstable = util.JSONToTable(body)
+				PrintTable(warnstable)
+			end,
+			function(message)
+				print(message)
+				requestPanelDataFromAPI()
+			end
+		)
+	end)
+
+	-- Kicks
+	timer.Create("PanelAPIRequestTimerKicks", 1, 0, function()
+		http.Fetch(PRIME_PANEL_CONFIG.URL .. "/api/?token=" .. PRIME_PANEL_CONFIG.SteamAPIKey .. "&id=" .. PRIME_PANEL_CONFIG.ID .. "&kicks=true",
+			function(body, length, headers, code)
+				print(body)
+				local kickstable = util.JSONToTable(body)
+				PrintTable(kickstable)
+			end,
+			function(message)
+				print(message)
+				requestPanelDataFromAPI()
+			end
+		)
+	end)
+
+	-- Bans
+	timer.Create("PanelAPIRequestTimerBans", 1, 0, function()
+		http.Fetch(PRIME_PANEL_CONFIG.URL .. "/api/?token=" .. PRIME_PANEL_CONFIG.SteamAPIKey .. "&id=" .. PRIME_PANEL_CONFIG.ID .. "&bans=true",
+			function(body, length, headers, code)
+				print(body)
+				local banstable = util.JSONToTable(body)
+				PrintTable(banstable)
 			end,
 			function(message)
 				print(message)
